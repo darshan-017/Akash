6693a88d7d708cf460de5768d3c3c8e29c9365af
darshan-017 <bdarshan363@gmail.com>
2023-09-03 23:37:27 +0530

    home page and contact page db

---
+++ photography/basic_web/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import ContactInfo, Photo
+# Register your models here.
+
+admin.site.register(ContactInfo)
+admin.site.register(Photo)
---
+++ photography/basic_web/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from datetime import datetime
+
+class Photo(models.Model):
+    CATEGORIES = [
+        ("animals", "Animals"),
+        ("weeding", "Weeding"),
+        ("birthday", "Birth Day"),
+        ("nature", "Nature"),
+    ]
+    name = models.CharField(max_length=60, null=True, blank=True)
+    category = models.CharField(max_length=15, choices=CATEGORIES)
+    date_added = models.DateTimeField(default=datetime.now, blank=True)
+    description = models.TextField()
+    photo = models.ImageField(upload_to="cars")
+
+    def __str__(self):
+        return self.name
+    
+class ContactInfo(models.Model):
+    name = models.CharField(max_length=60, null=True, blank=True)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField(max_length=100)
+    address = models.TextField(null=True, blank=True)
+    discuss_about = models.TextField(null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.name, self.email
